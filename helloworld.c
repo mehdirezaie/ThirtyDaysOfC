75bb1ab8e5697bdefbd758d8b3f3c737762f9681
Mehdi Rezaie <medirz90@icloud.com>
2019-12-30 22:43:42 -0500

    day 1

---
+++ helloworld.c
@@ -0,0 +1,21 @@
+/*
+    Credit : HACKERRANK.COM
+    Mehdi Rezaie 
+    Dec 30, 2019
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char input_string[105]; 
+    
+    scanf("%[^\n]", input_string); 
+    
+    printf("Hello, World.\n");
+    printf("%s\n", input_string); 
+    return 0;
+}
