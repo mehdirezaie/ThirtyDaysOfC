4eecbeba730cb2f31e672d2523c402197f7caee6
Mehdi Rezaie <medirz90@icloud.com>
2020-01-12 21:10:23 -0500

    add comment

--- Dictionary.py
+++ Dictionary.py
@@ -1,20 +1,30 @@
+'''
+Given n names and phone numbers, assemble a phone book 
+that maps friends' names to their respective phone numbers.
+You will then be given an unknown number of names to query 
+your phone book for. For each name queried, print the associated 
+entry from your phone book on a new line in the form name=phoneNumber;
+if an entry for name is not found, print Not found instead.
+
+'''
+
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 num = int(input())
 
 PhoneBook = {}
 for i in range(num):
     line = input().strip().split(' ')
-        PhoneBook[line[0]] = line[1]
+    PhoneBook[line[0]] = line[1]
 
-        Queries = []
-        while True:
-            try:
-                    Queries.append(input())
-                        except:
-                                break
+Queries = []
+while True:
+    try:
+        Queries.append(input())
+    except:
+        break
 
-                                for query in Queries:
-                                    if not query in PhoneBook:
-                                            print('Not found')
-                                                else:
-                                                        print(f'{query}={PhoneBook[query]}')
+for query in Queries:
+    if not query in PhoneBook:
+        print('Not found')
+    else:
+        print(f'{query}={PhoneBook[query]}')
