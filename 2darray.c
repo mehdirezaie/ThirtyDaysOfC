58ca6736e2caae2f82a58448634b0f29ec96f5ff
Mehdi Rezaie <medirz90@icloud.com>
2020-04-26 20:10:27 -0400

    2d array in c

---
+++ 2darray.c
@@ -0,0 +1,28 @@
+// 2D arrays in C
+// credit: https://overiq.com/c-programming-101/pointers-and-2-d-arrays/
+#include<stdio.h>
+
+int main()
+{
+    int arr[3][4] = {
+                        {11,22,33,44},
+                        {55,66,77,88},
+                        {11,66,77,44}
+                    };
+
+    int i, j;
+    int (*p)[4]; // pointer 
+
+    p = arr;
+
+    for(i = 0; i < 3; i++)
+    {
+        printf("Address of %d th array %u \n",i , p + i);
+        for(j = 0; j < 4; j++)
+        {
+            printf("arr[%d][%d]=%d\n", i, j, *( *(p + i) + j) );
+        }
+        printf("\n\n");
+    }
+    return 0;
+}
